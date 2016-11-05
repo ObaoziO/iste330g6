aaf5224e501dd3a08665b3509f673bc548934379
baozi <yqg900045@gmail.com>
2016-11-05 17:49:12 -0400

    Added empty DigitalLibrary and UserPermission classes

---
+++ DigitalLibrary.java
@@ -0,0 +1,13 @@
+/**
+ * Group 6: Yuqing Guo, David Player, Ashraf Wan
+ * Final Project
+ * ISTE.330.01
+ * Instructor: Michael Floeser
+**/
+
+// Main class: executing program methods
+public class DigitalLibrary {
+   public static void main(String[] args) {
+      
+   }
+}
