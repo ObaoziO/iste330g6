1612fb0956232c9b3ca2813539aa6491f6440598
Ashraffy <wxw9531@rit.edu>
2016-11-08 15:02:56 -0500

    Added Code
    Added the login code to the User Permission

--- UserPermission.java
+++ UserPermission.java
@@ -6,6 +6,32 @@
 **/
 
 // User specific permissions are provided accordingly
-public class UserPermission {
+public class UserPermission{
+   String fname = null;
+   String lname = null;
+   String email = null;
+   String pass = null;
+   LibraryDatabase ldb = new LibraryDatabase();
    
+   public boolean login(String inEmail, String inPass){
+      String[][] data = ldb.getData("SELECT * FROM faculty");
+      String test = null;
+      String test2 = null;
+      for(int i = 0; i < data.length; i++){
+         test = data[i][2];
+         test2 = data[i][3];
+         if(inEmail.equals(test2) && inPass.equals(test)){
+            email = inEmail;
+            pass = inPass;
+            fname = data[i][0];
+            lname = data[i][1];
+         }
+         else{
+            return false;
+         }
+         break;               
+      }
+      return true;
+
+   }
 }
