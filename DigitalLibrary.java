beef58bf9b4e1bf0b76ab0f155f73d8572ae63e3
baozi <yqg900045@gmail.com>
2016-11-28 10:26:04 -0500

    Moved GUI related objects from DifitalLibrary main class to GUI class

--- DigitalLibrary.java
+++ DigitalLibrary.java
@@ -6,53 +6,49 @@ import java.io.*;
 
 public class DigitalLibrary extends JFrame
 {
-   private JTextArea jtaTextArea; 
-   private JMenu jmAbout, jmSignIn, jmHelp, jmSearch; 
-   private JTextField jtfSearchBox; 
-   private JScrollPane scrollPane; 
-  
+
    public static void main(String [] args)
    {
-      new DigitalLibrary(); 
+      new GUI(); 
    }
-   public DigitalLibrary()
-   {
-      display();  
-   }
-   
-   /*Display GuI*/ 
-   public void display()
-   {  
-      JMenuBar topBar = new JMenuBar(); 
-      setJMenuBar(topBar);
-      
-      jmAbout = new JMenu("About"); 
-      topBar.add(jmAbout); 
-      
-      jmSignIn = new JMenu("Sign In"); 
-      topBar.add(jmSignIn); 
-      
-      jmHelp = new JMenu("Help"); 
-      topBar.add(jmHelp); 
-      
-      jtfSearchBox = new JTextField(20); 
-      
-      topBar.add(jtfSearchBox); 
-      topBar.add(Box.createHorizontalGlue());
-      jmSearch = new JMenu("Search");
-      topBar.add(jmSearch); 
-         
-      jtaTextArea = new JTextArea(25, 30); 
-      scrollPane = new JScrollPane(jtaTextArea); 
-     
-      add(scrollPane, BorderLayout.CENTER);
-      
-      setTitle("Digital Library for Research Collobrations"); 
-      setSize(600, 600); 
-      //pack();
-      setLocationRelativeTo( null );
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      setVisible(true);  
-   
-   }
+   // public DigitalLibrary()
+//    {
+//       display();  
+//    }
+//    
+//    /*Display GuI*/ 
+//    public void display()
+//    {  
+//       JMenuBar topBar = new JMenuBar(); 
+//       setJMenuBar(topBar);
+//       
+//       jmAbout = new JMenu("About"); 
+//       topBar.add(jmAbout); 
+//       
+//       jmSignIn = new JMenu("Sign In"); 
+//       topBar.add(jmSignIn); 
+//       
+//       jmHelp = new JMenu("Help"); 
+//       topBar.add(jmHelp); 
+//       
+//       jtfSearchBox = new JTextField(20); 
+//       
+//       topBar.add(jtfSearchBox); 
+//       topBar.add(Box.createHorizontalGlue());
+//       jmSearch = new JMenu("Search");
+//       topBar.add(jmSearch); 
+//          
+//       jtaTextArea = new JTextArea(25, 30); 
+//       scrollPane = new JScrollPane(jtaTextArea); 
+//      
+//       add(scrollPane, BorderLayout.CENTER);
+//       
+//       setTitle("Digital Library for Research Collobrations"); 
+//       setSize(600, 600); 
+//       //pack();
+//       setLocationRelativeTo( null );
+//       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//       setVisible(true);  
+//    
+//    }
 }
--- GUI.java
+++ GUI.java
@@ -10,29 +10,78 @@ import javax.swing.*;
 
 // **description of class here**
 public class GUI extends JFrame {
-   private JMenu jmTest;
-   private JMenuItem jmiTest;
+   private JTextArea jtaTextArea; 
+   private JMenu jmAbout, jmSignIn, jmHelp, jmSearch; 
+   private JTextField jtfSearchBox; 
+   private JScrollPane scrollPane; 
 
    public GUI() {
-      // Title of the window
-      setTitle("Digital Library for Research Collaborations");
-      
-      // Title area
-      JMenuBar jmbTest = new JMenuBar();
-      jmTest = new JMenu("Test menu");
-      jmiTest = new JMenuItem("Test item");
-      
-      jmTest.add(jmiTest);
-      
-      jmbTest.add(jmTest);
-      add(jmbTest);
-      
-      setJMenuBar(jmbTest);
-      
-      // Set GUI window size, location, visibility, etc.
-      setSize(800,550);
-      setLocationRelativeTo(null);
-      setDefaultCloseOperation(EXIT_ON_CLOSE);
-      setVisible(true);
+      display(); 
+      // // Title of the window
+   //       setTitle("Digital Library for Research Collaborations");
+   //       
+   //       // Title area
+   //       JMenuBar digitalLibraryJmb = new JMenuBar();
+   //       aboutJm = new JMenu("About");
+   //       signInJm = new JMenu("Sign In");
+   // //       helpJm = new JMenu("Help");
+   //       
+   //       // Add object to JMenuBar
+   //       digitalLibraryJmb.add(aboutJm);
+   //       digitalLibraryJmb.add(signInJm);
+   // //       digitalLibraryJmb.add(helpJm);
+   //       
+   //       // Add object to JFrame
+   //       add(digitalLibraryJmb);
+   //       
+   //       // ??
+   //       setJMenuBar(digitalLibraryJmb);
+   //       
+   //       // Set GUI window size, location, visibility, etc.
+   //       setSize(800,550);
+   //       setLocationRelativeTo(null);
+   //       setDefaultCloseOperation(EXIT_ON_CLOSE);
+   //       setVisible(true);
    }
+   
+   /*Display GUI*/ 
+   public void display()
+   {  
+      JMenuBar topBar = new JMenuBar(); 
+      setJMenuBar(topBar);
+      
+      // Create JMenu object
+      jmAbout = new JMenu("About"); 
+      topBar.add(jmAbout); 
+      
+      jmSignIn = new JMenu("Sign In"); 
+      topBar.add(jmSignIn); 
+      
+      jmHelp = new JMenu("Help"); 
+      topBar.add(jmHelp); 
+      
+      jtfSearchBox = new JTextField(20); 
+      
+      // Add object to JMenuBar
+      topBar.add(jtfSearchBox); 
+      topBar.add(Box.createHorizontalGlue());
+      jmSearch = new JMenu("Search");
+      topBar.add(jmSearch); 
+         
+      jtaTextArea = new JTextArea(25, 30); 
+      scrollPane = new JScrollPane(jtaTextArea); 
+     
+      // Add object to JFrame
+      add(scrollPane, BorderLayout.CENTER);
+      
+      // Set GUI property: title, window size, location, visibility, etc.
+      setTitle("Digital Library for Research Collobrations"); 
+      setSize(600, 600); 
+      //pack();
+      setLocationRelativeTo( null );
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setVisible(true);  
+   
+   }
+
 }
