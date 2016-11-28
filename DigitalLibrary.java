e9909d7b043b76c78ccb8f1160f2ff3bb3945bda
dap2154 <dap2154@rit.edu>
2016-11-28 09:01:52 -0500

    Add files via upload
    This Digital Library is only display the GUI without ActionListener. I can work on it during the our class time.

--- DigitalLibrary.java
+++ DigitalLibrary.java
@@ -1,14 +1,58 @@
-/**
- * Group 6: Yuqing Guo, David Player, Ashraf Wan
- * Final Project
- * ISTE.330.01
- * Instructor: Michael Floeser
-**/
+import java.awt.*; 
+import java.awt.event.*;
+import javax.swing.*; 
+import java.util.*;
+import java.io.*; 
 
-// Main class: executing program methods
-public class DigitalLibrary {
-   public static void main(String[] args) {
-      // Create new GUI object
-      new GUI();
+public class DigitalLibrary extends JFrame
+{
+   private JTextArea jtaTextArea; 
+   private JMenu jmAbout, jmSignIn, jmHelp, jmSearch; 
+   private JTextField jtfSearchBox; 
+   private JScrollPane scrollPane; 
+  
+   public static void main(String [] args)
+   {
+      new DigitalLibrary(); 
+   }
+   public DigitalLibrary()
+   {
+      display();  
+   }
+   
+   /*Display GuI*/ 
+   public void display()
+   {  
+      JMenuBar topBar = new JMenuBar(); 
+      setJMenuBar(topBar);
+      
+      jmAbout = new JMenu("About"); 
+      topBar.add(jmAbout); 
+      
+      jmSignIn = new JMenu("Sign In"); 
+      topBar.add(jmSignIn); 
+      
+      jmHelp = new JMenu("Help"); 
+      topBar.add(jmHelp); 
+      
+      jtfSearchBox = new JTextField(20); 
+      
+      topBar.add(jtfSearchBox); 
+      topBar.add(Box.createHorizontalGlue());
+      jmSearch = new JMenu("Search");
+      topBar.add(jmSearch); 
+         
+      jtaTextArea = new JTextArea(25, 30); 
+      scrollPane = new JScrollPane(jtaTextArea); 
+     
+      add(scrollPane, BorderLayout.CENTER);
+      
+      setTitle("Digital Library for Research Collobrations"); 
+      setSize(600, 600); 
+      //pack();
+      setLocationRelativeTo( null );
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setVisible(true);  
+   
    }
 }
