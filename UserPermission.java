aa3676321a2730906063f1570374d295858cbf07
baozi <yqg900045@gmail.com>
2016-11-28 10:39:29 -0500

    Cleaned up comments and edit files for format consistency forgot to save and add UserPermission.java

--- UserPermission.java
+++ UserPermission.java
@@ -6,7 +6,8 @@
 **/
 
 // User specific permissions are provided accordingly
-public class UserPermission {
+public class UserPermission 
+{
    String fname = null;
    String lname = null;
    String email = null;
@@ -14,20 +15,24 @@ public class UserPermission {
    LibraryDatabase ldb = new LibraryDatabase();
    
    // To check validity of user information
-   public boolean login(String inEmail, String inPass) {
+   public boolean login(String inEmail, String inPass) 
+   {
       String[][] data = ldb.getData("SELECT * FROM faculty");
       String test = null;
       String test2 = null;
-      for(int i = 0; i < data.length; i++) {
+      for(int i = 0; i < data.length; i++) 
+      {
          test = data[i][2];
          test2 = data[i][3];
-         if(inEmail.equals(test2) && inPass.equals(test)) {
+         if(inEmail.equals(test2) && inPass.equals(test)) 
+         {
             email = inEmail;
             pass = inPass;
             fname = data[i][0];
             lname = data[i][1];
          }
-         else {
+         else 
+         {
             return false;
          }
          break;               
