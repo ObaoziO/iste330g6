1007b06c321c892a6ae634322ab2e16cafad4f25
baozi <yqg900045@gmail.com>
2016-11-29 21:08:19 -0500

    Cleaned up comments

--- GUI.java
+++ GUI.java
@@ -5,30 +5,17 @@
 * Instructor: Michael Floeser
 **/
 
-// <<<<<<< HEAD
-// =======
 import java.awt.*;
 import java.awt.event.*;
-// >>>>>>> 2dca9acd054ae068178cfd95e30b875b4226faaa
 import javax.swing.*;
 import javax.swing.text.*;
-// import java.awt.event.*;
-// import java.awt.*;
 import java.sql.*;
 
 // **description of class here**
-// <<<<<<< HEAD
-// public class GUI extends JFrame implements ActionListener
-// {
-//    private JTextArea jtaTextArea; 
-//    private JMenu jmAbout, jmSignIn, jmHelp; // , jmSearch; 
-//    private JButton jbSearch;
-// =======
 public class GUI extends JFrame
 {
    private JTextArea jtaTextArea; 
    private JMenuItem jmiAbout, jmiSignIn, jmiHelp, jmiSearch; 
-//>>>>>>> 2dca9acd054ae068178cfd95e30b875b4226faaa
    private JTextField jtfSearchBox; 
    private JScrollPane scrollPane; 
    String users = null;
@@ -45,60 +32,52 @@ public class GUI extends JFrame
       JMenuBar topBar = new JMenuBar(); 
       setJMenuBar(topBar);
       
-      // Create JMenu and other objects to add to the JMenuBar
-// <<<<<<< HEAD
-//       jmAbout = new JMenu("About"); 
-//       jmSignIn = new JMenu("Sign In"); 
-//       jmHelp = new JMenu("Help"); 
-//       // jmSearch = new JMenu("Search");
-//       jbSearch = new JButton("Search");
-//       
-//       
-//       jtfSearchBox = new JTextField(15);
-//       //jbSearch.addActionListener(this);
-// =======
+      // Create JMenuItem and other objects to add to the JMenuBar
       jmiAbout = new JMenuItem("About");
-      jmiAbout.addActionListener(new ActionListener(){
-         public void actionPerformed(ActionEvent ae){
-            jtaTextArea.setText("Welcome to Our program");
-         }
-      });
+      jmiAbout.addActionListener(
+         new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+               jtaTextArea.setText("Welcome to Our program");
+            }
+         });
       jmiSignIn = new JMenuItem("Sign In");
-      jmiSignIn.addActionListener(new ActionListener(){
-         public void actionPerformed(ActionEvent ae){
-            JPanel overal = new JPanel(new BorderLayout(5, 5));
+      jmiSignIn.addActionListener(
+         new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+               JPanel overal = new JPanel(new BorderLayout(5, 5));
             
-            JPanel panel = new JPanel(new GridLayout(0,1,2,2));
-            panel.add(new JLabel("Username", SwingConstants.RIGHT));
-            panel.add(new JLabel("Password", SwingConstants.RIGHT));
-            overal.add(panel, BorderLayout.WEST);
+               JPanel panel = new JPanel(new GridLayout(0,1,2,2));
+               panel.add(new JLabel("Username", SwingConstants.RIGHT));
+               panel.add(new JLabel("Password", SwingConstants.RIGHT));
+               overal.add(panel, BorderLayout.WEST);
             
-            JPanel panel2 = new JPanel(new GridLayout(0,1,2,2));
-            JTextField username = new JTextField();
-            panel2.add(username);
-            JTextField password = new JTextField();
-            panel2.add(password);
-            overal.add(panel2, BorderLayout.CENTER);
+               JPanel panel2 = new JPanel(new GridLayout(0,1,2,2));
+               JTextField username = new JTextField();
+               panel2.add(username);
+               JTextField password = new JTextField();
+               panel2.add(password);
+               overal.add(panel2, BorderLayout.CENTER);
             
-            JOptionPane.showConfirmDialog(null,overal, "Sign-In", JOptionPane.OK_CANCEL_OPTION);
-            users = username.getText();
-            pass = password.getText(); 
-         }
-      });       
+               JOptionPane.showConfirmDialog(null,overal, "Sign-In", JOptionPane.OK_CANCEL_OPTION);
+               users = username.getText();
+               pass = password.getText(); 
+            }
+         });       
       jmiHelp = new JMenuItem("Help");
-      jmiHelp.addActionListener(new ActionListener(){
-         public void actionPerformed(ActionEvent ae){
-            JOptionPane.showMessageDialog(null, "Do you need help", "Help", JOptionPane.PLAIN_MESSAGE);
-         }
-      });       
+      jmiHelp.addActionListener(
+         new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+               JOptionPane.showMessageDialog(null, "Do you need help", "Help", JOptionPane.PLAIN_MESSAGE);
+            }
+         });       
       jmiSearch = new JMenuItem("Search");
-      jmiSearch.addActionListener(new ActionListener(){
-         public void actionPerformed(ActionEvent ae){
-            jtaTextArea.setText("Here are your search results");
-         }
-      });
+      jmiSearch.addActionListener(
+         new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+               jtaTextArea.setText("Here are your search results");
+            }
+         });
       jtfSearchBox = new JTextField(20); 
-//>>>>>>> 2dca9acd054ae068178cfd95e30b875b4226faaa
       
       // Add object to JMenuBar
       topBar.add(Box.createHorizontalGlue());
@@ -106,7 +85,6 @@ public class GUI extends JFrame
       topBar.add(jmiSignIn); 
       topBar.add(jmiHelp); 
       topBar.add(jtfSearchBox); 
-      //topBar.add(jbSearch);
       topBar.add(jmiSearch); 
          
       // jtaTextArea = new JTextArea(25, 30); 
@@ -134,15 +112,10 @@ public class GUI extends JFrame
    {
       
    }
-// <<<<<<< HEAD
    
    // Search the database to see if there are opportunities for collaboration using abstracts, keywords, etc.
    public void search(String input) { //throws FileNotFoundException 
       
    }
    
-
 } // GUI class
-// =======
-// }
-// >>>>>>> 2dca9acd054ae068178cfd95e30b875b4226faaa
